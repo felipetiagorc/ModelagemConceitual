67c0a4503ef667e179ba76396e8d1162360e21da
Felipe <felipetiagorc@yahoo.com.br>
2021-07-10 16:52:12 -0300

    Cliente, TipoCliente, Telefones, Enderecos

--- src/main/java/com/felipe/cursomc/repositories/EnderecoRepository.java
+++ src/main/java/com/felipe/cursomc/repositories/EnderecoRepository.java
@@ -3,10 +3,10 @@ package com.felipe.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.felipe.cursomc.domain.Categoria;
+import com.felipe.cursomc.domain.Endereco;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
 	
 
 }
