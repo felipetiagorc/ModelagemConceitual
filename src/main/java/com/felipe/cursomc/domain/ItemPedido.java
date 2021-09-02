654d558ab1a63876392c264a58918db85e4bedd2
Felipe Pereira <felipetiagorc@yahoo.com.br>
2021-09-02 13:23:36 -0300

    Endpoint /pedidos/1 ok - lindo

--- src/main/java/com/felipe/cursomc/domain/ItemPedido.java
+++ src/main/java/com/felipe/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	private Double desconto;
@@ -26,8 +29,14 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
+	}
+	
+//	@JsonIgnore = comentando isso aqui pra aparecer o Produto dentro do ItemPedido
+	public Produto getProduto() {
+		return id.getProduto();
 	}
 	
 	@Override
@@ -55,9 +64,6 @@ public class ItemPedido implements Serializable {
 		return true;
 	}
 
-	public Produto getProduto() {
-		return id.getProduto();
-	}
 	
 	
 	public ItemPedidoPK getId() {
--- src/main/java/com/felipe/cursomc/domain/Produto.java
+++ src/main/java/com/felipe/cursomc/domain/Produto.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -37,9 +38,11 @@ public class Produto implements Serializable {
 	
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
-		
+	
+	@JsonIgnore
 	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
 		// pra cada item de ProdutoXpedido na 'lista'
